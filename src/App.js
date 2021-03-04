02789e9b09d0c793d562f3a13c2c45bea71533e2
stratis-vip <stratis.vip@gmail.com>
2021-03-04 19:18:21 +0200

    all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import {graphqlMutation} from "aws-appsync-react"
+import {useState, useEffect} from "react"
 import './App.css';
+import gql from 'graphql-tag'
+import {graphql, compose }  from 'react-apollo'
+import {buildSubscription} from 'aws-appsync'
 
-function App() {
+const SubscribeToTodos = gql`
+    subscription {
+        onCreateTodo{
+            id title content completed
+        }
+    }
+`
+
+const ListTodos = gql`
+    query{
+        listTodos {
+            items {id title content }
+        }
+    }
+`
+
+const CreateTodo = gql`
+    mutation ($title:String!, $content: String!, $completed: Boolean){
+        createTodo(input:{
+            title: $title,
+            completed: $completed
+            content: $content
+        }){
+            id title content completed
+        }
+    }
+`
+const App = (props) => {
+  const [content, setContent] = useState('')
+  const {subscribeToMore} = props
+
+  useEffect(()=>{
+    subscribeToMore(
+      buildSubscription(SubscribeToTodos,ListTodos)
+    )
+  },[subscribeToMore])
+  console.log(props)
+
+  const addTodo = () => {
+    if (content === '') return
+    const newTodo = {title:'γαμήσια', content:content, completed: false}
+    props.createTodo(newTodo)
+    setContent('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type="text" onChange={(e)=>setContent(e.currentTarget.value)}
+        value={content}/>
+        <button onClick={addTodo}>Add</button>
+        {
+          props.todos.map((item,i)=>(<p key={i}>{item.content}</p>))
+        }
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default compose(
+  graphqlMutation(CreateTodo,ListTodos,'Todo'),
+  graphql(ListTodos, {
+    options: {
+      fetchPolicy: 'cache-and-network',
+    },
+    props: props => ({
+      subscribeToMore: props.data.subscribeToMore,
+      todos: props.data.listTodos.items ? props.data.listTodos.items : []
+    })
+  })
+)(App);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Client from 'aws-appsync';
+import {ApolloProvider} from 'react-apollo'
+import {Rehydrated} from 'aws-appsync-react'
+import awsmobile from './AppSync'
+
+const client = new Client({
+  url:awsmobile.aws_appsync_graphqlEndpoint,
+  region:awsmobile.aws_appsync_region,
+  auth:{
+    type: "API_KEY",
+    apiKey: awsmobile.aws_appsync_apiKey,
+  }
+})
+
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WithProvider />
   </React.StrictMode>,
   document.getElementById('root')
 );
